f7cb414f683f71c61ba8020837abb768f80b8c87
CodeByAdriel <adriel.almeida.dev@gmail.com>
2024-12-16 08:36:31 -0300

    correção do import no main route

--- src/main.routes.ts
+++ src/main.routes.ts
@@ -1,12 +1,11 @@
 import express, { Router } from 'express';
 import leadsRoutes from './leads/leads.routes';
+import usuariosRoutes from './usuarios/usuarios.routes';
 
 const router: Router = express.Router();
 
 // Rotas de leads (prefixo /leads)
 router.use('/leads', leadsRoutes);
-
-// Outras rotas podem ser adicionadas futuramente:
-// router.use('/usuarios', usuariosRoutes);
+router.use('/usuarios', usuariosRoutes)
 
 export default router;
